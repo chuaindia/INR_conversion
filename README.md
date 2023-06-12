68a310818c55b51c4008fa961e0ab6d8de9c7f3d
Tanusri Ghosh <106825373+chuaindia@users.noreply.github.com>
2023-06-12 21:08:41 +0530

    Update JavaScript file

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # INR_conversion
+
+This is a simple app which convert different currencies in Indian currency INR.
--- app.js
+++ app.js
@@ -1,5 +1,20 @@
-function convertToInr(){
-    let usd = document.getElementById('usd').value;
-    let inr = usd * 82.5;
-    document.getElementById('inr').innerText = inr;
+function convertCurrency() {
+    let amount = document.getElementById('amount').value;
+    let currency = document.getElementById('currency').value;
+  
+    // Define the conversion rates for different currencies
+    const conversionRates = {
+        USD: 82.5,
+        EUR: 97.8,
+        GBP: 113.2,
+        KS: 0.59,
+        TAKA: 0.76,
+        // Add more currencies and their conversion rates here
+    };
+  
+    // Convert the amount to INR using the specified currency code
+    let result = amount * conversionRates[currency];
+  
+    // Update the result on the HTML element with id 'result'
+    document.getElementById('result').innerText = `${result} INR`;
 }
